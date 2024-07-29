5eb98a2385aa6b94669118a69452f7cc360b173c
Y-RyuZU <ryuzu1006@gmail.com>
2024-07-29 14:54:46 -0400

    feature: FTBQuestsの翻訳でchapter_groupsも翻訳するように

--- src/init.py
+++ src/init.py
@@ -6,9 +6,10 @@ BETTERQUESTING_DIR = Path('./resources/betterquesting/lang')
 FTBQUESTS_DIR1 = Path('./kubejs/assets/kubejs/lang')
 FTBQUESTS_DIR2 = Path('./kubejs/assets/ftbquests/lang')
 FTBQUESTS_DIR3 = Path('./config/ftbquests/quests/chapters')
+FTBQUESTS_DIR4 = Path('./config/ftbquests/quests/chapter_groups.snbt')
 
 MAX_ATTEMPTS = 5
 
 USER = 'Y-RyuZU'
 REPO = 'MinecraftModsLocalizer'
-VERSION = 'v2.0.5'
+VERSION = 'v2.0.6'
