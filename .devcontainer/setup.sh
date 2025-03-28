90952d457cfa717b0ef28e9b70a00f70cb346681
Y-RyuZU <ryuzu1006@gmail.com>
2025-03-28 21:45:28 +0900

    feature: add devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt install -y libwebkit2gtk-4.1-dev \
+    build-essential \
+    curl \
+    wget \
+    file \
+    libxdo-dev \
+    libssl-dev \
+    libayatana-appindicator3-dev \
+    librsvg2-dev
